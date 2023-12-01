24d4ac86c5c5fa01f3af0a607d93bf648a6e9891
unknown <yrezehi@gmail.com>
2023-11-30 21:40:57 -0800

    DatasetSpliter

---
+++ Embedder/Embedder/Datasets/Dataset.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Embedder.Datasets
+{
+    public class Dataset
+    {
+        public IList<Pair> Train;
+        public IList<Pair> Validation;
+        public IList<Pair> Test;
+
+        public Dataset() { }
+        public Dataset(IList<Pair> train, IList<Pair> validation, IList<Pair> test) =>
+            (Train, Validation, Test) = (train, validation, test);
+
+        public static Dataset Instance(IList<Pair> train, IList<Pair> validation, IList<Pair> test) =>
+            new Dataset(train, validation, test);
+        public static Dataset Instance() =>
+            new Dataset();
+    }
+}
---
+++ Embedder/Embedder/Datasets/DatasetSpliter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Embedder.Datasets
+{
+    public class DatasetSpliter
+    {
+        public static Dataset Split(IList<Pair> unsplittedDataset)
+        {
+            Dataset dataset = Dataset.Instance();
+
+            int trainLength = (int)Math.Round(unsplittedDataset.Count * .70);
+            int validtionLength = (int)Math.Round(unsplittedDataset.Count * .20);
+
+            dataset.Train = unsplittedDataset.Take(trainLength).ToList();
+            dataset.Validation = unsplittedDataset.Skip(trainLength).Take(validtionLength - trainLength).ToList();
+            dataset.Test = unsplittedDataset.Skip(validtionLength).ToList();
+
+            return dataset;
+        }
+    }
+}
