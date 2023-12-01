4c2d33e470a8a5181de94228ecd716a6dda3f4b7
unknown <yrezehi@gmail.com>
2023-11-30 16:45:59 -0800

    finish pair

--- Embedder/Embedder/Datasets/DatasetLoader.cs
+++ Embedder/Embedder/Datasets/DatasetLoader.cs
@@ -1,6 +1,37 @@
-﻿namespace Embedder.Datasets
+﻿using Microsoft.VisualBasic.FileIO;
+
+namespace Embedder.Datasets
 {
     public class DatasetLoader
     {
+        private string Path;
+
+        private static string DELIMITERS = ",";
+
+        public DatasetLoader(string path) =>
+            Path = path;
+
+        public IList<Pair> Load()
+        {
+            var pairs = new List<Pair>();
+
+            using (TextFieldParser parser = new TextFieldParser)
+            {
+                parser.SetDelimiters(DELIMITERS);
+                parser.HasFieldsEnclosedInQuotes = true;
+
+                parser.ReadLine();
+
+                while (!parser.EndOfData)
+                {
+                    pairs.Add(new Pair());
+                }
+            }
+        }
+
+        public static DatasetLoader Load(string path) =>
+            new DatasetLoader(path);
+      
+
     }
 }
--- Embedder/Embedder/Datasets/Pair.cs
+++ Embedder/Embedder/Datasets/Pair.cs
@@ -1,9 +1,16 @@
-﻿namespace Embedder.Datasets
+﻿using System.Reflection.Emit;
+
+namespace Embedder.Datasets
 {
     public class Pair
     {
         public string FirstPair { get;set; }
         public string SecondPair { get;set; }
         public int Label { get;set; }
+
+        public Pair(string firstPair, string secondPair, int label) =>
+            (FirstPair, SecondPair, Label) = (firstPair, secondPair, label);
+        public static Pair Create(string firstPair, string secondPair, int label) =>
+            new Pair(firstPair, secondPair, label);
     }
 }
